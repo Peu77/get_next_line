55c0f566f9310b559bcbcf55e12b67e5cc4cf234
Emil Ebert <emilebert@icloud.com>
2024-09-25 23:11:02 +0200

    clean code and made 42 norm correct

--- get_next_line.c
+++ get_next_line.c
@@ -1,68 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eebert <eebert@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 23:08:51 by eebert            #+#    #+#             */
+/*   Updated: 2024/09/25 23:10:06 by eebert           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
 #include <malloc.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include "get_next_line.h"
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 10000000
 #endif
 
-char *get_next_line(int fd) {
-    static char *lineBuffer = NULL;
+int	read_file(int fd, char **lineBuffer, char *buffer)
+{
+	int		read_bytes;
+	char	*temp;
 
-    char *buffer;
-    int bytesRead = 0;
-    int totalBytesRead = 0;
-    char *new_line_pos;
+	read_bytes = read(fd, buffer, BUFFER_SIZE);
+	if (read_bytes <= 0)
+		return (read_bytes);
+	buffer[read_bytes] = '\0';
+	if (*lineBuffer == NULL)
+		*lineBuffer = ft_strndup(buffer, read_bytes);
+	else
+	{
+		temp = ft_strjoin(*lineBuffer, buffer);
+		free(*lineBuffer);
+		*lineBuffer = temp;
+	}
+	return (read_bytes);
+}
 
-    buffer = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    while ((new_line_pos = ft_strchr(lineBuffer + totalBytesRead, '\n')) == NULL) {
-        totalBytesRead += bytesRead;
-        bytesRead = read(fd, buffer, BUFFER_SIZE);
-        if (bytesRead <= 0) {
-            break;
-        }
-        buffer[bytesRead] = '\0';
-        if (lineBuffer == NULL)
-            lineBuffer = ft_strndup(buffer, (size_t) bytesRead);
-        else {
-            char *temp = ft_strjoin(lineBuffer, buffer);
-            free(lineBuffer);
-            lineBuffer = temp;
-        }
-    }
-    free(buffer);
+char	*get_result(char **lineBuffer, char *new_line_pos)
+{
+	char	*line;
+	char	*line;
+	char	*temp;
 
-    if (bytesRead == -1)
-        return (NULL);
+	if (new_line_pos == NULL)
+	{
+		if (*lineBuffer == NULL || **lineBuffer == '\0')
+			return (NULL);
+		line = ft_strndup(*lineBuffer, (size_t)ft_strlen(*lineBuffer));
+		free(*lineBuffer);
+		*lineBuffer = NULL;
+		return (line);
+	}
+	line = ft_strndup(*lineBuffer, new_line_pos - *lineBuffer + 1);
+	temp = ft_strndup(new_line_pos + 1, (size_t)ft_strlen(new_line_pos + 1));
+	free(*lineBuffer);
+	*lineBuffer = temp;
+	return (line);
+}
 
-    if (bytesRead == 0 && lineBuffer == NULL)
-        return (NULL);
+char	*get_next_line(int fd)
+{
+	static char	*line_buffer = NULL;
+	char		*buffer;
+	int			bytes_read;
+	int			total_bytes_read;
+	char		*new_line_pos;
 
-    if (new_line_pos == NULL) {
-        if (lineBuffer == NULL || *lineBuffer == '\0')
-            return (NULL);
-        char *line = ft_strndup(lineBuffer, (size_t) ft_strlen(lineBuffer));
-        free(lineBuffer);
-        lineBuffer = NULL;
-        return (line);
-    }
-
-    char *line = ft_strndup(lineBuffer, new_line_pos - lineBuffer + 1);
-    char *temp = ft_strndup(new_line_pos + 1, (size_t) ft_strlen(new_line_pos + 1));
-    free(lineBuffer);
-    lineBuffer = temp;
-    return (line);
+	bytes_read = 1;
+	total_bytes_read = -1;
+	new_line_pos = NULL;
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (new_line_pos == NULL && bytes_read > 0)
+	{
+		total_bytes_read += bytes_read;
+		bytes_read = read_file(fd, &line_buffer, buffer);
+		new_line_pos = ft_strchr(line_buffer + total_bytes_read, '\n');
+	}
+	free(buffer);
+	return (get_result(&line_buffer, new_line_pos));
 }
 
 /*
-int main() {
-    char *line;
-    int fd = open("../get_next_line.c", O_RDONLY);
-    line = get_next_line(fd);
-    printf("%s\n", line);
-    free(line);
-    close(fd);
-    return (0);
+int	main(void) {
+	char *line;
+	int fd = open("../big_line_no_nl", O_RDONLY);
+	line = get_next_line(fd);
+	printf("%s\n", line);
+	free(line);
+	close(fd);
+	return (0);
 }
- */
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eebert <eebert@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 23:10:16 by eebert            #+#    #+#             */
+/*   Updated: 2024/09/25 23:10:18 by eebert           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
+# include <stdlib.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 
-char *ft_strchr(const char *str, int c);
+char	*ft_strchr(const char *str, int c);
 
-char *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 
-char *ft_strlen(const char *s);
+char	*ft_strlen(const char *s);
 
-char *ft_strndup(const char *src, size_t n);
+char	*ft_strndup(const char *src, size_t n);
 
 #endif
