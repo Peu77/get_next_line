2945dbb1b04d16171f00eb3144d0cf970f6386af
Emil Ebert <emilebert@icloud.com>
2024-09-25 23:30:38 +0200

    fix norm error

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eebert <eebert@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 23:10:16 by eebert            #+#    #+#             */
-/*   Updated: 2024/09/25 23:10:18 by eebert           ###   ########.fr       */
+/*   Updated: 2024/09/25 23:30:27 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 
@@ -23,14 +23,14 @@
 #  define BUFFER_SIZE 42
 # endif
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 
-char *ft_strchr(const char *str, int c);
+char	*ft_strchr(const char *str, int c);
 
-char *ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char const *s1, char const *s2);
 
-char *ft_strlen(const char *s);
+char	*ft_strlen(const char *s);
 
-char *ft_strndup(const char *src, size_t n);
+char	*ft_strndup(const char *src, size_t n);
 
 #endif
