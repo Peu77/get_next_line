05a29bd3e52cf2f3c9b00e76097b0455f88ce20b
Emil Ebert <eebert@2-G-7.42heilbronn.de>
2024-10-08 13:01:39 +0200

    fix mem leak

--- get_next_line.c
+++ get_next_line.c
@@ -6,86 +6,121 @@
 /*   By: eebert <eebert@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 23:08:51 by eebert            #+#    #+#             */
-/*   Updated: 2024/09/25 23:10:06 by eebert           ###   ########.fr       */
+/*   Updated: 2024/10/08 13:00:32 by eebert           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <malloc.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int read_file(int fd, char **lineBuffer, char *buffer) {
-    int read_bytes;
-    char *temp;
+int	read_file(int fd, char **line_buffer, char *buffer)
+{
+	int		read_bytes;
+	char	*temp;
 
-    read_bytes = read(fd, buffer, BUFFER_SIZE);
-    if (read_bytes <= 0)
-        return (read_bytes);
-    buffer[read_bytes] = '\0';
-    if (*lineBuffer == NULL)
-        *lineBuffer = ft_strndup(buffer, read_bytes);
-    else {
-        temp = ft_strjoin(*lineBuffer, buffer);
-        free(*lineBuffer);
-        *lineBuffer = temp;
-    }
-    return (read_bytes);
+	read_bytes = read(fd, buffer, BUFFER_SIZE);
+	if (read_bytes <= 0)
+	{
+		if (read_bytes == -1)
+		{
+			free(*line_buffer);
+			*line_buffer = NULL;
+			free(buffer);
+		}
+		return (read_bytes);
+	}
+	buffer[read_bytes] = '\0';
+	if (*line_buffer == NULL)
+		*line_buffer = ft_strndup(buffer, read_bytes);
+	else
+	{
+		temp = ft_strjoin(*line_buffer, buffer);
+		free(*line_buffer);
+		*line_buffer = temp;
+	}
+	return (read_bytes);
 }
 
-char *get_result(char **lineBuffer, char *new_line_pos) {
-    char *line;
-    char *temp;
-
-    if (*lineBuffer == NULL) {
-        return (NULL);
-    }
-
-    if (**lineBuffer == '\0') {
-        free(*lineBuffer);
-        *lineBuffer = NULL;
-        return (NULL);
-    }
-
-    if (new_line_pos == NULL) {
-        line = ft_strndup(*lineBuffer, (size_t) ft_strlen(*lineBuffer));
-        free(*lineBuffer);
-        *lineBuffer = NULL;
-        return (line);
-    }
-    line = ft_strndup(*lineBuffer, new_line_pos - *lineBuffer + 1);
-    temp = ft_strndup(new_line_pos + 1, (size_t) ft_strlen(new_line_pos + 1));
-    free(*lineBuffer);
-    if (line == NULL || temp == NULL) {
-        free(line);
-        free(temp);
-        return (NULL);
-    }
-    *lineBuffer = temp;
-    return (line);
+void	handle_zero_case(char **line_buffer)
+{
+	if (*line_buffer == NULL)
+		return ;
+	if (**line_buffer == '\0')
+	{
+		free(*line_buffer);
+		*line_buffer = NULL;
+	}
 }
 
-char *get_next_line(int fd) {
-    static char *line_buffer = NULL;
-    char *buffer;
-    int bytes_read;
-    int total_bytes_read;
-    char *new_line_pos;
+char	*get_result(char **line_buffer, char *new_line_pos)
+{
+	char	*line;
+	char	*temp;
 
-    bytes_read = 1;
-    total_bytes_read = -1;
-    new_line_pos = NULL;
-    buffer = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (!buffer) {
-        free(line_buffer);
-        line_buffer = NULL;
-        return (NULL);
-    }
-    while (new_line_pos == NULL && bytes_read > 0) {
-        total_bytes_read += bytes_read;
-        bytes_read = read_file(fd, &line_buffer, buffer);
-        new_line_pos = ft_strchr(line_buffer + total_bytes_read, '\n');
-    }
-    free(buffer);
-    return (get_result(&line_buffer, new_line_pos));
+	if (*line_buffer == NULL || **line_buffer == '\0')
+		return (handle_zero_case(line_buffer), NULL);
+	if (new_line_pos == NULL)
+	{
+		line = ft_strndup(*line_buffer, (size_t)ft_strlen(*line_buffer));
+		free(*line_buffer);
+		*line_buffer = NULL;
+		return (line);
+	}
+	line = ft_strndup(*line_buffer, new_line_pos - *line_buffer + 1);
+	temp = ft_strndup(new_line_pos + 1, (size_t)ft_strlen(new_line_pos + 1));
+	free(*line_buffer);
+	if (line == NULL || temp == NULL)
+	{
+		free(line);
+		free(temp);
+		return (NULL);
+	}
+	*line_buffer = temp;
+	return (line);
 }
+
+char	*get_next_line(int fd)
+{
+	static char	*line_buffer = NULL;
+	char		*buffer;
+	int			bytes_read;
+	int			total_bytes_read;
+	char		*new_line_pos;
+
+	bytes_read = 1;
+	total_bytes_read = -1;
+	new_line_pos = NULL;
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+	{
+		free(line_buffer);
+		line_buffer = NULL;
+		return (NULL);
+	}
+	while (new_line_pos == NULL && bytes_read > 0)
+	{
+		total_bytes_read += bytes_read;
+		bytes_read = read_file(fd, &line_buffer, buffer);
+		if (bytes_read == -1)
+			return (NULL);
+		new_line_pos = ft_strchr(line_buffer + total_bytes_read, '\n');
+	}
+	return ((free(buffer)), get_result(&line_buffer, new_line_pos));
+}
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("get_next_line.c", O_RDONLY);
+// 	while ((line = get_next_line(7)))
+// 	{
+// 		printf("%s\n", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
